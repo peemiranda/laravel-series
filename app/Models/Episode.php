e0154f10acd49e07f9355f74d0ec04e2eb528d98
peemiranda <pedrohmiranda@proton.me>
2024-02-04 23:39:07 -0300

    Add Episode model

---
+++ app/Models/Episode.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Episode extends Model
+{
+    use HasFactory;
+    protected $fillable = ['number'];
+    public $timestamps = false;
+
+    public function seasons()
+    {
+        return $this->belongsTo(Season::class);
+    }
+
+}
