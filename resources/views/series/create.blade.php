bc797abf06972d5b73774e63d616a6bdb72317e0
peemiranda <pedrohmiranda@proton.me>
2024-02-04 23:40:14 -0300

    Refactor series create form

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,10 +1,35 @@
 <x-layout title="Nova serie">
-    <form action="/series/salvar" method="post">
+    <form action="{{ route('series.store') }}" method="post">
         @csrf
-        <div class="mb-3">
-        <label for="nome" class="form-label"> Nome: </label>
-        <input type="text" class="form-control" id="nome" name="nome">
+
+        <div class="row mb-3">
+            <div class="col-8">
+                <label for="name" class="form-label"> Nome: </label>
+                <input type="text"
+                       autofocus
+                       id="name"
+                       name="name"
+                       class="form-control"
+                       value="{{ old('name') }}">
+            </div>
+            <div class="col-2">
+                <label for="seasonQty" class="form-label"> Numero de temporadas: </label>
+                <input type="text"
+                       id="seasonQty"
+                       name="seasonQty"
+                       class="form-control"
+                       value="{{ old('name') }}">
+            </div>
+            <div class="col-2">
+                <label for="episodesPerSeason" class="form-label"> Episodios por temporada: </label>
+                <input type="text"
+                       id="episodesPerSeason"
+                       name="episodesPerSeason"
+                       class="form-control"
+                       value="{{ old('name') }}">
+            </div>
         </div>
+
         <button type="submit" class="btn btn-primary"> Adicionar </button>
     </form>
 </x-layout>
